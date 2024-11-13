333e0c373b5c6f30089eb455f2fb50f9124da63c
matttrach <matt.trachier@suse.com>
2024-11-13 14:29:29 -0600

    fix: append certificate chain to the certificate
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- examples/one/outputs.tf
+++ examples/one/outputs.tf
@@ -1,18 +1,18 @@
 output "kubeconfig" {
-  value       = module.this.kubeconfig
+  value       = module.rancher.kubeconfig
   description = <<-EOT
     The kubeconfig for the server.
   EOT
   sensitive   = true
 }
 output "address" {
-  value = module.this.address
+  value = module.rancher.address
 }
 output "admin_token" {
-  value     = module.this.admin_token
+  value     = module.rancher.admin_token
   sensitive = true
 }
 output "admin_password" {
-  value     = module.this.admin_password
+  value     = module.rancher.admin_password
   sensitive = true
 }
