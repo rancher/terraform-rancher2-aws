378057ebc8d1e58dca0108f485f4d2217dd064dd
matttrach <matt.trachier@suse.com>
2025-01-10 10:46:31 -0600

    fix: enable automatic testing and refactor tests
    Signed-off-by: matttrach <matt.trachier@suse.com>

---
+++ examples/import_rke2/modules/cluster/outputs.tf
@@ -0,0 +1,39 @@
+output "kubeconfig" {
+  value       = local.ino.kubeconfig
+  description = "Kubernetes config file contents for the cluster."
+  sensitive   = true
+}
+output "api" {
+  value       = yamldecode(local.ino.kubeconfig).clusters[0].cluster.server
+  description = "Address to use to connect to the cluster's API service."
+  sensitive   = true
+}
+output "cert" {
+  value       = local.ino.project_domain_tls_certificate
+  description = "Information about the certificate that was generated with the domain"
+  sensitive   = true
+}
+# commented for performance, leaving to show how you can export the state if necessary
+# output "additional_node_states" {
+#   value       = data.terraform_remote_state.additional_node_states
+#   description = "The states for the orchestrated modules which produce nodes."
+#   sensitive   = true
+# }
+output "vpc" {
+  value = module.initial[keys(local.initial_node)[0]].project_vpc
+}
+output "subnets" {
+  value = module.initial[keys(local.initial_node)[0]].project_subnets
+}
+output "join_url" {
+  value = local.ino.join_url
+}
+output "initial_node_private_ip" {
+  value = replace(replace(local.ino.join_url, ":9345", ""), "https://", "")
+}
+output "project_domain_object" {
+  value = module.initial[keys(local.initial_node)[0]].project_domain_object
+}
+output "project_security_group" {
+  value = module.initial[keys(local.initial_node)[0]].project_security_group
+}
---
+++ examples/import_rke2/modules/rancher/main.tf
@@ -0,0 +1,65 @@
+locals {
+  identifier              = var.identifier
+  example                 = "basic"
+  project_name            = "tf-${substr(md5(join("-", [local.example, local.identifier])), 0, 5)}"
+  username                = local.project_name
+  domain                  = local.project_name
+  zone                    = var.zone
+  key_name                = var.key_name
+  key                     = var.key
+  owner                   = var.owner
+  rke2_version            = var.rke2_version
+  local_file_path         = var.file_path
+  runner_ip               = chomp(data.http.myip.response_body) # "runner" is the server running Terraform
+  rancher_version         = var.rancher_version
+  rancher_helm_repository = "https://releases.rancher.com/server-charts/stable"
+  cert_manager_version    = "v1.13.1"
+  os                      = "sle-micro-60"
+}
+
+data "http" "myip" {
+  url = "https://ipinfo.io/ip"
+}
+
+module "rancher" {
+  source = "../../"
+  # project
+  identifier   = local.identifier
+  owner        = local.owner
+  project_name = local.project_name
+  domain       = local.domain
+  zone         = local.zone
+  # access
+  key_name = local.key_name
+  key      = local.key
+  username = local.username
+  admin_ip = local.runner_ip
+  # rke2
+  rke2_version    = local.rke2_version
+  local_file_path = local.local_file_path
+  install_method  = "rpm" # rpm only for now, need to figure out local helm chart installs otherwise
+  cni             = "canal"
+  node_configuration = {
+    "rancher" = {
+      type            = "all-in-one"
+      size            = "medium"
+      os              = local.os
+      indirect_access = true
+      initial         = true
+    }
+  }
+  # rancher
+  cert_manager_version    = local.cert_manager_version
+  rancher_version         = local.rancher_version
+  rancher_helm_repository = local.rancher_helm_repository
+}
+
+# test catalog entry
+resource "rancher2_catalog" "foo" {
+  depends_on = [
+    module.rancher,
+  ]
+  provider = rancher2.default
+  name     = "test"
+  url      = "http://foo.com:8080"
+}
