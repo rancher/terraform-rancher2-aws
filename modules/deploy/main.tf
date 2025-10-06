c10b76bb8ec913de4003960c74b73fc3663f58a0
matttrach <matt.trachier@suse.com>
2025-10-06 16:20:55 -0500

    feat: implement the file provider
    Signed-off-by: matttrach <matt.trachier@suse.com>

--- modules/deploy/main.tf
+++ modules/deploy/main.tf
@@ -3,134 +3,270 @@
 # I felt this was the best way to accomplish the goal without incurring additional dependencies
 
 locals {
-  inputs         = var.inputs
-  inputs_hash    = md5(local.inputs)
-  template_path  = var.template_path
-  template_files = var.template_files
-  # tflint-ignore: terraform_unused_declarations
-  fail_no_template = ((local.template_path == null && length(local.template_files) == 0) ? one([local.template_path, "missing_template"]) : false)
-  # tflint-ignore: terraform_unused_declarations
-  fail_too_much_template = ((local.template_path != null && length(local.template_files) > 0) ? one([local.template_path, "template_path_or_template_files"]) : false)
-  template_file_list = (
-    local.template_path != null ?
-    [
-      for i in range(length(fileset(local.template_path, "**"))) :
-      join("/", [local.template_path, tolist(fileset(local.template_path, "**"))[i]])
-    ]
-    : local.template_files
-  )
-  template_file_map   = { for file in local.template_file_list : basename(file) => file }
-  template_files_hash = md5(join("-", local.template_file_list))
-  deploy_path         = chomp(var.deploy_path)
+  template_files    = var.template_files
+  template_file_map = { for file in local.template_files : basename(file) => file }
 
-  environment_variables = var.environment_variables
+  # template_file_map     = { for i in range(length(local.template_files)) : tostring(i) => local.template_files[i] }
+  # need to figure out how to copy the files sent, the for_each loop won't work due to the dynamic read of the directory
+  inputs                = var.inputs
+  environment_variables = merge(var.environment_variables, { "TF_DATA_DIR" = local.tf_data_dir })
   export_contents = (
     local.environment_variables != null ?
     join(";", [for k, v in local.environment_variables : "export ${k}=${v}"])
     : ""
   )
-  export_hash  = md5(local.export_contents)
-  attempts     = var.attempts
-  interval     = var.interval
-  timeout      = var.timeout
-  init         = var.init
-  init_script  = (local.init ? "terraform init -reconfigure -upgrade" : "")
-  tf_data_dir  = var.data_path != null ? var.data_path : path.root
-  skip_destroy = (var.skip_destroy ? "true" : "")
+
+  deploy_trigger = var.deploy_trigger
+  deploy_path    = chomp(var.deploy_path)
+  attempts       = var.attempts
+  interval       = var.interval
+  timeout        = var.timeout
+  init           = var.init
+  init_script    = (local.init ? "terraform init -reconfigure -upgrade" : "")
+  tf_data_dir    = (var.data_path != null ? var.data_path : path.root)
+  skip_destroy   = (var.skip_destroy ? "true" : "")
 }
 
-module "persist_template" {
-  source = "../persist_file"
-  depends_on = [
-  ]
-  for_each = local.template_file_map
-  path     = "${local.deploy_path}/${each.key}"
-  contents = file(each.value)
-  recreate = filemd5(each.value)
+resource "file_local_directory" "deploy_path" {
+  path        = local.deploy_path
+  permissions = "0755"
 }
 
-module "persist_inputs" {
-  source = "../persist_file"
-  depends_on = [
-  ]
-  path     = "${local.deploy_path}/inputs.tfvars"
-  contents = local.inputs
-  recreate = md5(local.inputs)
+resource "file_local_directory" "tf_data_dir" {
+  count       = (local.tf_data_dir != local.deploy_path ? 1 : 0)
+  path        = local.tf_data_dir
+  permissions = "0755"
 }
+
+### Template Files ###
+data "file_local" "template_files" {
+  for_each  = local.template_file_map
+  directory = dirname(each.value)
+  name      = each.key
+}
+resource "file_local_snapshot" "persist_tpl_file" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    data.file_local.template_files,
+  ]
+  for_each       = local.template_file_map
+  directory      = dirname(each.value)
+  name           = each.key
+  update_trigger = local.deploy_trigger
+}
+resource "file_local" "instantiate_tpl_snapshot" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    data.file_local.template_files,
+    file_local_snapshot.persist_tpl_file,
+  ]
+  for_each    = local.template_file_map
+  directory   = local.deploy_path
+  name        = each.key
+  permissions = data.file_local.template_files[each.key].permissions
+  contents    = base64decode(file_local_snapshot.persist_tpl_file[each.key].snapshot)
+}
+
+### Inputs ###
+resource "terraform_data" "write_tmp_inputs" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+  ]
+  triggers_replace = {
+    trigger = local.deploy_trigger
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      cat <<'EOF'> "${local.tf_data_dir}/inputs"
+      ${local.inputs}
+      EOF
+    EOT
+  }
+}
+resource "file_local_snapshot" "persist_inputs" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_inputs,
+  ]
+  directory      = local.tf_data_dir
+  name           = "inputs"
+  update_trigger = local.deploy_trigger
+}
+resource "terraform_data" "remove_tmp_inputs" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_inputs,
+    file_local_snapshot.persist_inputs,
+  ]
+  triggers_replace = {
+    trigger = local.deploy_trigger
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      rm -f "${local.tf_data_dir}/inputs"
+    EOT
+  }
+}
+resource "file_local" "instantiate_inputs_snapshot" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_inputs,
+    file_local_snapshot.persist_inputs,
+    terraform_data.remove_tmp_inputs,
+  ]
+  directory = local.deploy_path
+  name      = "inputs.tfvars"
+  contents  = base64decode(file_local_snapshot.persist_inputs.snapshot)
+}
+
+### Environment Variables ###
+resource "terraform_data" "write_tmp_envrc" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+  ]
+  triggers_replace = {
+    trigger = local.deploy_trigger
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      cat <<'EOF'> "${local.tf_data_dir}/envrc"
+      ${local.export_contents}
+      EOF
+    EOT
+  }
+}
+resource "file_local_snapshot" "persist_envrc" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_envrc,
+  ]
+  directory      = local.tf_data_dir
+  name           = "envrc"
+  update_trigger = local.deploy_trigger
+}
+resource "terraform_data" "remove_tmp_envrc" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_envrc,
+    file_local_snapshot.persist_envrc,
+  ]
+  triggers_replace = {
+    trigger = local.deploy_trigger
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      rm -f "${local.tf_data_dir}/envrc"
+    EOT
+  }
+}
+resource "file_local" "instantiate_envrc_snapshot" {
+  depends_on = [
+    file_local_directory.deploy_path,
+    file_local_directory.tf_data_dir,
+    terraform_data.write_tmp_envrc,
+    file_local_snapshot.persist_envrc,
+    terraform_data.remove_tmp_envrc,
+  ]
+  directory = local.deploy_path
+  name      = "envrc"
+  contents  = base64decode(file_local_snapshot.persist_envrc.snapshot)
+}
+
 
 resource "terraform_data" "destroy" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
+    file_local.instantiate_envrc_snapshot,
+    file_local.instantiate_inputs_snapshot,
+    file_local.instantiate_tpl_snapshot,
   ]
   triggers_replace = {
-    inputs = local.inputs_hash
-    files  = local.template_files_hash
-    env    = local.export_hash
-    ec     = local.export_contents
-    dp     = local.deploy_path
-    to     = local.timeout
-    dd     = local.tf_data_dir
-    sd     = local.skip_destroy
+    trigger = local.deploy_trigger
+    dp      = local.deploy_path
+    sd      = local.skip_destroy
+    to      = local.timeout
   }
   provisioner "local-exec" {
     when = destroy
     command = templatefile("${path.module}/destroy.sh.tpl", {
-      export_contents = self.triggers_replace.ec
-      tf_data_dir     = self.triggers_replace.dd
-      deploy_path     = self.triggers_replace.dp
-      skip_destroy    = self.triggers_replace.sd
-      timeout         = self.triggers_replace.to
+      deploy_path  = self.triggers_replace.dp
+      skip_destroy = self.triggers_replace.sd
+      timeout      = self.triggers_replace.to
     })
   }
 }
 
 resource "terraform_data" "create" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
+    file_local.instantiate_envrc_snapshot,
+    file_local.instantiate_inputs_snapshot,
+    file_local.instantiate_tpl_snapshot,
     terraform_data.destroy,
   ]
   triggers_replace = {
-    files = local.template_files_hash
+    never = <<-EOT
+      This resource is only meant to run once,
+        on the initial deploy,
+        the secondary create manages updates.
+    EOT
   }
   provisioner "local-exec" {
     command = templatefile("${path.module}/create.sh.tpl", {
-      export_contents = local.export_contents
-      deploy_path     = local.deploy_path
-      tf_data_dir     = local.tf_data_dir
-      init_script     = local.init_script
-      attempts        = local.attempts
-      timeout         = local.timeout
-      interval        = local.interval
+      deploy_path = local.deploy_path
+      init_script = local.init_script
+      attempts    = local.attempts
+      timeout     = local.timeout
+      interval    = local.interval
     })
   }
 }
 
-module "persist_state" {
+resource "file_local_snapshot" "persist_state" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
     terraform_data.destroy,
     terraform_data.create,
   ]
-  source     = "../persist_file"
-  path       = "${local.deploy_path}/tfstate"
-  sourcefile = "${local.deploy_path}/tfstate"
-  recreate   = terraform_data.create.id
+  directory      = local.deploy_path
+  name           = "tfstate"
+  update_trigger = terraform_data.create.id
+}
+resource "file_local" "instantiate_state" {
+  depends_on = [
+    terraform_data.destroy,
+    terraform_data.create,
+    file_local_snapshot.persist_state,
+  ]
+  directory = local.deploy_path
+  name      = "tfstate"
+  contents  = base64decode(file_local_snapshot.persist_state.snapshot)
 }
 
-module "persist_outputs" {
+resource "file_local_snapshot" "persist_outputs" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
     terraform_data.destroy,
     terraform_data.create,
   ]
-  source     = "../persist_file"
-  path       = "${local.deploy_path}/outputs.json"
-  sourcefile = "${local.deploy_path}/outputs.json"
-  recreate   = terraform_data.create.id
+  directory      = local.deploy_path
+  name           = "outputs.json"
+  update_trigger = terraform_data.create.id
+}
+resource "file_local" "instantiate_outputs" {
+  depends_on = [
+    terraform_data.destroy,
+    terraform_data.create,
+    file_local_snapshot.persist_outputs,
+  ]
+  directory = local.deploy_path
+  name      = "outputs.json"
+  contents  = base64decode(file_local_snapshot.persist_outputs.snapshot)
 }
 
 # during initial create this should be an extra apply that has no effect
@@ -138,59 +274,50 @@ module "persist_outputs" {
 #  to rebuild the template before running the create script
 resource "terraform_data" "create_after_persist" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
+    file_local.instantiate_envrc_snapshot,
+    file_local.instantiate_inputs_snapshot,
+    file_local.instantiate_tpl_snapshot,
     terraform_data.destroy,
     terraform_data.create,
-    module.persist_state,
-    module.persist_outputs,
+    file_local.instantiate_state,
+    file_local.instantiate_outputs,
   ]
   triggers_replace = {
-    inputs = local.inputs_hash
-    files  = local.template_files_hash
-    env    = local.export_hash
+    trigger = local.deploy_trigger
   }
   provisioner "local-exec" {
     command = templatefile("${path.module}/create.sh.tpl", {
-      export_contents = local.export_contents
-      deploy_path     = local.deploy_path
-      tf_data_dir     = local.tf_data_dir
-      init_script     = local.init_script
-      attempts        = local.attempts
-      timeout         = local.timeout
-      interval        = local.interval
+      deploy_path = local.deploy_path
+      init_script = local.init_script
+      attempts    = local.attempts
+      timeout     = local.timeout
+      interval    = local.interval
     })
   }
 }
 
 resource "terraform_data" "destroy_end" {
   depends_on = [
-    module.persist_template,
-    module.persist_inputs,
+    file_local.instantiate_envrc_snapshot,
+    file_local.instantiate_inputs_snapshot,
+    file_local.instantiate_tpl_snapshot,
     terraform_data.destroy,
     terraform_data.create,
-    module.persist_state,
-    module.persist_outputs,
+    file_local.instantiate_state,
+    file_local.instantiate_outputs,
     terraform_data.create_after_persist,
   ]
   triggers_replace = {
-    inputs = local.inputs_hash
-    files  = local.template_files_hash
-    env    = local.export_hash
-    ec     = local.export_contents
-    dp     = local.deploy_path
-    to     = local.timeout
-    dd     = local.tf_data_dir
-    sd     = local.skip_destroy
+    dp = local.deploy_path
+    sd = local.skip_destroy
+    to = local.timeout
   }
   provisioner "local-exec" {
     when = destroy
     command = templatefile("${path.module}/destroy.sh.tpl", {
-      export_contents = self.triggers_replace.ec
-      tf_data_dir     = self.triggers_replace.dd
-      deploy_path     = self.triggers_replace.dp
-      skip_destroy    = self.triggers_replace.sd
-      timeout         = self.triggers_replace.to
+      deploy_path  = self.triggers_replace.dp
+      skip_destroy = self.triggers_replace.sd
+      timeout      = self.triggers_replace.to
     })
   }
 }
