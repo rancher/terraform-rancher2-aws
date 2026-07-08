629f30b3b950215b5cc1b230ce70eacf836351d1
Matt Trachier <matt.trachier@suse.com>
2026-07-08 16:39:17 -0500

    fix: format nix script
    Signed-off-by: Matt Trachier <matt.trachier@suse.com>

--- .github/workflows/scripts/nix-shell.sh
+++ .github/workflows/scripts/nix-shell.sh
@@ -3,24 +3,24 @@ set -e
 
 source /home/suse/.profile
 
-nix develop
-  --ignore-environment
-  --extra-experimental-features nix-command
-  --extra-experimental-features flakes
-  --keep HOME
-  --keep SSH_AUTH_SOCK
-  --keep GITHUB_TOKEN
-  --keep AWS_ROLE
-  --keep AWS_REGION
-  --keep AWS_DEFAULT_REGION
-  --keep AWS_ACCESS_KEY_ID
-  --keep AWS_SECRET_ACCESS_KEY
-  --keep AWS_SESSION_TOKEN
-  --keep UPDATECLI_GPGTOKEN
-  --keep UPDATECLI_GITHUB_TOKEN
-  --keep UPDATECLI_GITHUB_ACTOR
-  --keep GPG_SIGNING_KEY
-  --keep NIX_SSL_CERT_FILE
-  --keep NIX_ENV_LOADED
-  --keep TERM
-  --command bash -e {0}
+nix develop \
+  --extra-experimental-features nix-command \
+  --extra-experimental-features flakes \
+  --ignore-environment \
+  --impure \
+  --keep HOME \
+  --keep SSH_AUTH_SOCK \
+  --keep GITHUB_TOKEN \
+  --keep AWS_ROLE \
+  --keep AWS_REGION \
+  --keep AWS_DEFAULT_REGION \
+  --keep AWS_ACCESS_KEY_ID \
+  --keep AWS_SECRET_ACCESS_KEY \
+  --keep AWS_SESSION_TOKEN \
+  --keep KUBE_CONFIG_PATH \
+  --keep TERM \
+  --keep XDG_DATA_DIRS \
+  --keep NIX_SSL_CERT_FILE \
+  --keep NIX_PROFILE \
+  --profile "$profile" \
+  --command bash -c "{0}"
